27bca76f12a38240df76d7ca86da74115b111792
Daniel Macharia <danielhuios@gmail.com>
2022-09-24 21:54:14 +0300

    add transaction feature added

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -11,13 +11,33 @@ function AccountContainer() {
 		fetch("http://localhost:8001/transactions")
 			.then((res) => res.json())
 			//get data and store it in a state
-			.then((data) => setTransactions(data));
+			.then((data) => setTransactions(data))
+			.catch((error) => console.log(error));
 	}, []);
+
+	function handleSubmissionUpdate(newSubmission) {
+		//get newSubmission data and add to current transactions
+		//post newSubmission data
+
+		fetch("http://localhost:8001/transactions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newSubmission),
+		})
+			.then((res) => res.json())
+			.then((newData) =>
+				setTransactions((transactions) => [...transactions, newData])
+			)
+			.catch((error) => console.log(error));
+	}
+
 	console.log(transactions);
 	return (
 		<div>
 			<Search />
-			<AddTransactionForm />
+			<AddTransactionForm submission={handleSubmissionUpdate} />
 			<TransactionsList transactions={transactions} />
 		</div>
 	);
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
-  return (
-    <div className="ui segment">
-      <form className="ui form">
-        <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
-        </div>
-        <button className="ui button" type="submit">
-          Add Transaction
-        </button>
-      </form>
-    </div>
-  );
+function AddTransactionForm({ submission }) {
+	const [formData, setFormData] = useState({
+		date: "",
+		description: "",
+		category: "",
+		amount: 0,
+	});
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		submission(formData);
+	}
+
+	function handleChange(e) {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	}
+
+	return (
+		<div className="ui segment">
+			<form onChange={handleChange} onSubmit={handleSubmit} className="ui form">
+				<div className="inline fields">
+					<input defaultValue={formData.date} type="date" name="date" />
+					<input
+						defaultValue={formData.description}
+						type="text"
+						name="description"
+						placeholder="Description"
+					/>
+					<input
+						defaultValue={formData.category}
+						type="text"
+						name="category"
+						placeholder="Category"
+					/>
+					<input
+						defaultValue={formData.amount}
+						type="number"
+						name="amount"
+						placeholder="Amount"
+						step="0.01"
+					/>
+				</div>
+				<button className="ui button" type="submit">
+					Add Transaction
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default AddTransactionForm;
