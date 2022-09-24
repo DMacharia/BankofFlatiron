1b4ee78b9a33c759823a0b6843f94f70d27b188d
Daniel Macharia <danielhuios@gmail.com>
2022-09-24 10:40:49 +0300

    fetched data to the transactions list

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  return (
-    <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
-    </div>
-  );
+	//use useEffect hook to fetch data
+	//create stateful transaction variables
+	const [transactions, setTransactions] = useState([]);
+	useEffect(() => {
+		fetch("http://localhost:8001/transactions")
+			.then((res) => res.json())
+			//get data and store it in a state
+			.then((data) => setTransactions(data));
+	}, []);
+	console.log(transactions);
+	return (
+		<div>
+			<Search />
+			<AddTransactionForm />
+			<TransactionsList transactions={transactions} />
+		</div>
+	);
 }
 
 export default AccountContainer;
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,43 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {/* render a list of <Transaction> components here */}
-      </tbody>
-    </table>
-  );
+function TransactionsList({ transactions }) {
+	const transactionItems = transactions.map((transaction) => {
+		return (
+			<Transaction
+				key={transaction.id}
+				date={transaction.date}
+				description={transaction.description}
+				category={transaction.category}
+				amount={transaction.amount}
+			/>
+		);
+	});
+	console.log(transactionItems);
+
+	return (
+		<table className="ui celled striped padded table">
+			<tbody>
+				<tr>
+					<th>
+						<h3 className="ui center aligned header">Date</h3>
+					</th>
+					<th>
+						<h3 className="ui center aligned header">Description</h3>
+					</th>
+					<th>
+						<h3 className="ui center aligned header">Category</h3>
+					</th>
+					<th>
+						<h3 className="ui center aligned header">Amount</h3>
+					</th>
+				</tr>
+
+				{/* transaction items here */}
+				{transactionItems}
+			</tbody>
+		</table>
+	);
 }
 
 export default TransactionsList;
